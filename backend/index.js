3b0d511ef162819eb35745730fe5519f50e6479d
Usu <usuedeaghe@gmail.com>
2023-10-10 17:12:55 +0100

    Hello World Route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
