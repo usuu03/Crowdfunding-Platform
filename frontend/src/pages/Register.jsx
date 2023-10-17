ea475e2c6bf775ed66bdc9c87d744d2abf1dbf94
Usu <usuedeaghe@gmail.com>
2023-10-17 16:21:13 +0100

    Initial Login and Register UI set up

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div>
+      <h2>Register Page</h2>
+
+      <form className="" action="">
+        <div className="form-name">
+          <label htmlFor="">Name:</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
+}
