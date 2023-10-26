ed75b88207abb21a66c3095d7d65049700a0b33a
Usu <usuedeaghe@gmail.com>
2023-10-26 20:18:38 +0100

    installing jwt and bcrypt packages

---
+++ backend/controllers/authController.js
@@ -0,0 +1,12 @@
+const db = require("../config/dbConfig");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const login = (req, res) => {};
+
+const register = (req, res) => {};
+
+module.exports = {
+  login,
+  register,
+};
