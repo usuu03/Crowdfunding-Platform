8f6bc75b393a95a14ce5c7ed485cbfb1b7130c08
christopher <co377@kent.ac.uk>
2023-10-31 15:29:00 +0000

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// src/SearchBar.js
+import React, { useState } from 'react';
+import SearchBar from './searchbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> new page.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar/>
+
     </div>
-  );
+  )
 }
 
 export default App;
