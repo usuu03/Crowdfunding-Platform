38f2aad5dc2a8d3953bc239b387d9214c5d92110
Usu <usuedeaghe@gmail.com>
2023-11-06 15:39:18 +0000

    registration controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,36 @@ const db = require("../config/dbConfig");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const register = async (req, res) => {
+  try {
+    const { firstName, lastName, emailAddress, password } = req.body;
+
+    //Encrypting the password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    db.query(
+      "INSERT INTO User (firstName, lastName, emailAddress, password) VALUES (?, ?, ?, ?)",
+      [firstName, lastName, emailAddress, hashPassword],
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ message: "Registration Unsuccessful" });
+        }
+
+        res.status(201).json({ message: "Registration successful" });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 const login = async (req, res) => {
   try {
     const { emailAddress, password } = req.body;
@@ -29,7 +59,7 @@ const login = async (req, res) => {
         }
 
         // Generate a JWT token
-        const token = jwt.sign({ emailAddress }, "your-secret-key", {
+        const token = jwt.sign({ emailAddress }, process.env.JWT_SECRET_KEY, {
           expiresIn: "1h",
         });
 
@@ -39,19 +69,6 @@ const login = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Login Unsuccessful", token });
-  }
-};
-
-const register = async (req, res) => {
-  try {
-    const { firstName, lastName, emailAddress, password } = req.body;
-
-    db.query(
-      "INSERT INTO table_name(firstName, lastName, ) VALUES (value_1,value_2,value_3)"
-    );
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Registration Unsuccessful", token });
   }
 };
 
