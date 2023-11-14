7b13bd882bc782bd0d4a52c1fd7cd5aee98913f9
Usu <usuedeaghe@gmail.com>
2023-11-14 15:03:27 +0000

    discovery page integration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
       console.log(response.data);
 
       //Redirecting to the Homepage if successfully logged in
-      navigate("/home");
+      navigate("/discover");
     } catch (error) {
       console.error("Login Error:", error);
     }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -47,7 +47,7 @@ export default function Register() {
         userData
       );
       console.log(response.data);
-      navigate("/home");
+      navigate("/discover");
     } catch (error) {
       console.error("Registration error:", error);
     }
