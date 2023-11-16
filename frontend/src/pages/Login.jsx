0ac3ea642bc8833b1b90b689f436e6188bec6c70
Abdul <aasl2@kent.ac.uk>
2023-11-16 14:20:11 +0000

    Intergrate

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
       console.log(response.data);
 
       //Redirecting to the Homepage if successfully logged in
-      navigate("/discover");
+      navigate("/discovery");
     } catch (error) {
       console.error("Login Error:", error);
     }
