c0ae12a70d02a492582fd137122cbca5354970fc
Usu <usuedeaghe@gmail.com>
2023-11-21 13:02:34 +0000

    implementing authentication context to page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthState, useAuthDispatch } from "../context/authContext"; // Import your authentication context and hooks
 
 export default function Header() {
+  const { isAuthenticated, user } = useAuthState(); // Get authentication state
+  const authDispatch = useAuthDispatch(); // Get authentication dispatch function
+
+  const handleLogout = () => {
+    // Dispatch a logout action when the user clicks "Logout"
+    authDispatch({ type: "LOGOUT" });
+  };
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
@@ -21,20 +30,35 @@ export default function Header() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
+            {/* <li className="nav-item active">
               <Link to="/create-campaign" className="nav-link" id="nav-item">
                 Create
               </Link>
-            </li>
+            </li> */}
+
             <li className="nav-item">
               <Link to="/discovery" className="nav-link" id="nav-item">
                 Discovery
               </Link>
             </li>
+
             <li className="nav-item">
-              <Link to="/login" className="nav-link" id="nav-item">
-                Login/Logout
+              <Link to="/campaigns" className="nav-link" id="nav-item">
+                My Campaigns
               </Link>
+            </li>
+
+            <li className="nav-item">
+              {/* Conditionally render "Login" or "Logout" based on authentication status */}
+              {isAuthenticated ? (
+                <span className="nav-link" id="nav-item" onClick={handleLogout}>
+                  Logout
+                </span>
+              ) : (
+                <Link to="/login" className="nav-link" id="nav-item">
+                  Login
+                </Link>
+              )}
             </li>
           </ul>
         </div>
