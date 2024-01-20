8fa597ae8284b74e3635871b4b43bcb3a2241b96
Usu <usuedeaghe@gmail.com>
2024-01-20 19:49:01 +0000

    search bar UI update

--- frontend/src/pages/SearchBar.jsx
+++ frontend/src/pages/SearchBar.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
+import { Button, Card, Form, InputGroup, ProgressBar } from "react-bootstrap";
+import "../styles/searchbar.css";
 
 // SearchBar component for searching campaigns
 export default function SearchBar() {
@@ -33,61 +35,55 @@ export default function SearchBar() {
     (currentAmount / goal) * 100 + "%";
 
   return (
-    <div className="container d-flex flex-column align-items-center">
+    <div className="container">
       {/* Search title */}
       <h2 className="search-title">Search Campaigns</h2>
 
       {/* Search form */}
-      <form onSubmit={handleSearch} className="form-div">
-        <div className="d-flex align-items-center">
+      <div className="searchbar">
+        <form onSubmit={handleSearch} className="form-div">
           {/* Search input */}
-          <input
-            type="search"
-            name="searchQuery"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="form-control mr-sm-2"
-            placeholder="Search Campaigns..."
-            id="search-bar"
-          />
 
-          {/* Search button */}
-          <button type="submit" className="btn btn-primary">
-            Search
-          </button>
-        </div>
-      </form>
+          <InputGroup>
+            <Form.Control
+              onSubmit={handleSearch}
+              placeholder="Search Campaings..."
+              size="lg"
+              type="search"
+              name="searchQuery"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <Button type="submit">Search Campaigns</Button>
+          </InputGroup>
+        </form>
+      </div>
 
       {/* Display search results */}
       <div className="campaign-container">
         {searchResults.map((campaign) => (
-          <div key={campaign.id} className="campaign-box">
-            <div className="campaign-box-content">
-              {/* Campaign image */}
-              <div className="campaign-image">
-                <img src="image-placeholder.jpg" alt="Campaign Image" />
-              </div>
-              {/* Campaign details */}
-              <div id={`campaign-${campaign.id}`}>
-                <h3>{campaign.campaignTitle}</h3>
-                <p>
-                  Raised: ${campaign.currentAmount} of ${campaign.goal}
-                </p>
-                {/* Progress bar */}
-                <div className="progress">
-                  <div
-                    className="progress-bar"
-                    style={{
-                      width: progressWidth(
-                        campaign.currentAmount,
-                        campaign.goal
-                      ),
-                    }}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Card border="light" id={campaign.campaignID}>
+            <Card.Img
+              variant="top"
+              src={`http://localhost:4000/uploads/${campaign.posterImage}`}
+            />
+
+            <Card.Body id={`campaign-${campaign.campaignID}`}>
+              <Card.Title>{campaign.campaignTitle}</Card.Title>
+
+              <Card.Text>
+                Raised: ${campaign.currentAmount} of ${campaign.goal}
+              </Card.Text>
+
+              {/* Progress Bar */}
+              <ProgressBar
+                now={(campaign.currentAmount / campaign.goal) * 100}
+                label={`${Math.round(
+                  (campaign.currentAmount / campaign.goal) * 100
+                )}%`}
+              />
+            </Card.Body>
+          </Card>
         ))}
       </div>
     </div>
