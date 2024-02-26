72bab92a3c88a0627532d216f144e88fdb7462a0
Usu <usuedeaghe@gmail.com>
2024-02-26 23:24:05 +0000

    debit payment UI

--- frontend/src/pages/PaymentForm.jsx
+++ frontend/src/pages/PaymentForm.jsx
@@ -1,43 +1,102 @@
-import React from "react";
-import { Button, Container, Form, InputGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 
 export default function PaymentForm() {
+  const [selectedAmount, setSelectedAmount] = useState(0);
+  const handleAmountClick = (amount) => {
+    setSelectedAmount(amount);
+  };
   return (
     <Container>
       <div className="payment">
-        <h2>Donate to Campaign</h2>
+        <h2>Hello there!</h2>
+        <h6>How much would you like to donate?</h6>
         <div className="payment-form">
           <Form>
-            <Form.Control
-              placeholder="Amount to Donate"
-              type="number"
-              className="form-item"
-            />
+            {/* Amount Options */}
+            <div className="amount-options">
+              <Button
+                id="amount-btn"
+                variant={selectedAmount === 25 ? "primary" : "outline-primary"}
+                onClick={() => handleAmountClick(25)}
+              >
+                £25
+              </Button>
+              <Button
+                id="amount-btn"
+                variant={selectedAmount === 50 ? "primary" : "outline-primary"}
+                onClick={() => handleAmountClick(50)}
+              >
+                £50
+              </Button>
+              <Button
+                id="amount-btn"
+                variant={selectedAmount === 100 ? "primary" : "outline-primary"}
+                onClick={() => handleAmountClick(100)}
+              >
+                £100
+              </Button>
+              <Button
+                id="amount-btn"
+                variant={selectedAmount === 250 ? "primary" : "outline-primary"}
+                onClick={() => handleAmountClick(250)}
+              >
+                £250
+              </Button>
+              <Button
+                id="amount-btn"
+                variant={selectedAmount === 500 ? "primary" : "outline-primary"}
+                onClick={() => handleAmountClick(500)}
+              >
+                £500
+              </Button>
+              <Button
+                id="amount-btn"
+                variant={
+                  selectedAmount === 1000 ? "primary" : "outline-primary"
+                }
+                onClick={() => handleAmountClick(1000)}
+              >
+                £1000
+              </Button>
+            </div>
+
             <hr />
             <div className="credit-information">
-              <h5>Credit/Debit Information</h5>
-              <Form.Control
-                type="text"
-                placeholder="Full Name"
-                required
-                className="form-item"
-              />
-              <Form.Control
-                type="email"
-                placeholder="Email Address"
-                className="form-item"
-                required
-              />
-              <Form.Control
-                type="text"
-                placeholder="Card Number"
-                required
-                className="form-item"
-              />
-              <InputGroup className="form-item">
-                <Form.Control type="text" placeholder="MM/YY" required />
-                <Form.Control type="text" placeholder="CVV" required />
-              </InputGroup>
+              <h6>Personal Info</h6>
+              <div className="name-email">
+                <div className="name">
+                  <Form.Label>Full Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="John Doe"
+                    required
+                    className="form-item"
+                  />
+                </div>
+
+                <div className="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="johndoe@gmail.com"
+                    className="form-item"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="address-door">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="123, Kingston Streets"
+                  required
+                  className="form-item"
+                />
+                <Form.Label>Apt/Door</Form.Label>
+                <Form.Control type="text" placeholder="4A" required />
+              </div>
+
               <Button variant="outline-primary">Make a Donation</Button>
             </div>
           </Form>
