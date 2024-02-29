22ed516221dae026ca515ccb1e63785b12b75343
Abdul <aasl2@kent.ac.uk>
2024-02-29 10:20:06 +0000

    new

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -2,11 +2,20 @@ const nodemailer = require('nodemailer');
 
 // Create a nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'smtp.live.com',
+  service: 'smtp.office365.com',
+  port: 465,
+  secure: true,
+  logger: true,
+  debug: true,
+  secureConnection: false,
   auth: {
-    user: 'helpinghandcfp@outlook.com',
-    pass: 'HelpingHand1@'
+    user: "helpinghandcfp@outlook.com",
+    pass: "HelpingHand1@"
+  },
+  tls:{
+    rejectUnauthorized: true
   }
+
 });
 
 const sendEmail = async (email, subject, html) => {
