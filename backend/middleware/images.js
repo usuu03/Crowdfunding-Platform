a3eece37563380ed0332ebf1ec6a8e44f0e3d746
Usu <usuedeaghe@gmail.com>
2024-03-05 19:31:18 +0000

    cloudinary api integration

--- backend/middleware/images.js
+++ backend/middleware/images.js
@@ -1,47 +1,36 @@
 // uploadMiddleware.js
 // import { v2 as cloudinary } from "cloudinary";
+const { error } = require("console");
 const multer = require("multer");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
 
-// cloudinary.config({
-//   cloud_name: "djaylhbph",
-//   api_key: "149185691639867",
-//   api_secret: "jkBm42de6S5CBFzt3tT-lbucqv4",
-// });
-
-// Define storage for multer
-const storage = multer.diskStorage({
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+cloudinary.config({
+  cloud_name: "djaylhbph",
+  api_key: "149185691639867",
+  api_secret: "jkBm42de6S5CBFzt3tT-lbucqv4",
 });
 
-// Create multer instance
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Middleware function to upload image to Cloudinary
 const uploadImage = (req, res, next) => {
   upload.single("posterImage")(req, res, async (err) => {
     if (err) {
-      console.error("Error uploading image:", err);
+      console.error("Error uploading image: ", err);
       return res.status(500).json({ error: "Failed to upload image" });
     }
-
     try {
       if (!req.file) {
         return res.status(400).json({ error: "No image file provided" });
       }
 
       // Upload image to Cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
+      const result = await cloudinary.uploader.upload(req.file.buffer);
 
       // Set the Cloudinary URL as the file path
       req.file.path = result.secure_url;
 
-      // Call next middleware
       next();
     } catch (error) {
       console.error("Error uploading image to Cloudinary:", error);
@@ -52,6 +41,49 @@ const uploadImage = (req, res, next) => {
   });
 };
 
-module.exports = {
-  uploadImage: uploadImage,
-};
+// // Define storage for multer
+// const storage = multer.diskStorage({
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+
+// // Create multer instance
+// const upload = multer({ storage: storage });
+
+// // Middleware function to upload image to Cloudinary
+// const uploadImage = (req, res, next) => {
+//   upload.single("posterImage")(req, res, async (err) => {
+//     if (err) {
+//       console.error("Error uploading image:", err);
+//       return res.status(500).json({ error: "Failed to upload image" });
+//     }
+
+//     try {
+//       if (!req.file) {
+//         return res.status(400).json({ error: "No image file provided" });
+//       }
+
+//       // Upload image to Cloudinary
+//       const result = await cloudinary.uploader.upload(req.file.path);
+
+//       // Set the Cloudinary URL as the file path
+//       req.file.path = result.secure_url;
+
+//       // Call next middleware
+//       next();
+//     } catch (error) {
+//       console.error("Error uploading image to Cloudinary:", error);
+//       return res
+//         .status(500)
+//         .json({ error: "Failed to upload image to Cloudinary" });
+//     }
+//   });
+// };
+
+// module.exports = {
+//   uploadImage: uploadImage,
+// };
